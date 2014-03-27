d4181f25363e65635b5bfecf6e2a7d28a50ed3cc
achingbrain <alex@achingbrain.net>
2014-03-27 08:06:59 +0000

    adding and removing files

--- test/S3Test.js
+++ test/S3Test.js
@@ -3,6 +3,8 @@ var should = require("should"),
 	os = require("os"),
 	fs = require("fs"),
 	path = require("path"),
+	request = require("request"),
+	async = require("async"),
 	S3 = require("../lib/S3");
 
 describe("S3", function() {
@@ -90,7 +92,53 @@ describe("S3", function() {
 		done();
 	})
 
-	it("should store a file", function(done) {
-		done();
+	it("should store and remove a file", function(done) {
+
+		// if you want to run this test, remove the next line and add your S3 details below
+		return done();
+
+		var bucket =  "PUT_YOUR_BUCKET_HERE";
+		var key = "PUT_YOUR_KEY_HERE";
+		var secret = "PUT_YOUR_BUCKET_HERE";
+		var region = "PUT_YOUR_REGION_HERE";
+		var sourceFile = path.resolve(__dirname + "/./fixtures/node_js_logo.png");
+
+		var s3 = new S3({
+			key: key,
+			secret: secret,
+			bucket: bucket,
+			region: region
+		});
+
+		var s3Url;
+
+		async.waterfall([function(callback) {
+			// save the file
+			s3.save(sourceFile, callback);
+		}, function(url, callback) {
+			s3Url = url;
+
+			// make sure it was uploaded
+			request.head(url, callback);
+		}, function(response, body, callback) {
+			// resource should exist
+			response.statusCode.should.equal(200);
+
+			// remove the file
+			s3.remove({url: s3Url}, callback);
+		}, function(message, callback) {
+			// make sure it's not there any more
+			request.head(s3Url, callback);
+		}, function(response, body, callback) {
+			// resource should exist
+			response.statusCode.should.not.equal(200);
+
+			// all done
+			callback();
+		}], function(error) {
+			should(error).not.ok;
+
+			done();
+		});
 	})
 })
